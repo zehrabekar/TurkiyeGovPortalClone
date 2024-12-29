e4a91e42598ea17653167bb891fd0ca6c026d519
Zehra Bekar <zehrabekar54@gmail.com>
2024-12-30 01:09:05 +0300

    The positions of the buttons in the navbar have been adjusted and the responsive design for the header has been completed.

--- project.js
+++ project.js
@@ -8,10 +8,28 @@ window.addEventListener('scroll', function () {
     }
   });
 
+  // Sayfa kaydırıldığında butonun görünürlüğünü kontrol et
+window.addEventListener('scroll', function () {
+  let newButton2 = document.getElementById('newButton2');
+  if (window.scrollY > 340) { // 443px kaydırıldığında
+    newButton2.style.display = 'inline'; // Butonu göster
+  } else {
+    newButton2.style.display = 'none'; // Butonu gizle
+  }
+});
+
 // Navbar toggle butonuna tıklandığında header'ı genişlet
 document.querySelector('.navbar-toggler').addEventListener('click', function () {
     let header = document.getElementById('header');
     header.classList.toggle('expand');  // 'expand' sınıfını ekler veya çıkarır
+});
+
+// Header yüksekliğini sayfa genişliğine göre ayarla
+window.addEventListener('resize', function() {
+  let header = document.getElementById('header');
+  if (window.innerWidth > 767) {  // Eğer sayfa genişliği 767px'yi geçtiyse
+    header.classList.remove('expand');  // Header'ı 80px yap
+  }
 });
 
   
