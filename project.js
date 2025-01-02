cffe5fb73f654435839f3fb3e7c14bdf13fd5af1
Zehra Bekar <zehrabekar54@gmail.com>
2025-01-03 02:28:34 +0300

    Added items for two sections and implement responsive design

--- project.js
+++ project.js
@@ -32,4 +32,37 @@ window.addEventListener('resize', function() {
   }
 });
 
-  
+window.addEventListener('resize', updateLayout);
+window.addEventListener('load', updateLayout);
+
+function updateLayout() {
+  let actionsBlock = document.getElementById('actions-block');
+  let actionsList = document.getElementById('actions-list');
+  let actionsList2 = document.getElementById('actions-list2');
+  let windowWidth = window.innerWidth;
+
+  if (windowWidth <= 767) {
+    actionsBlock.style.height = '410px';
+    actionsList.style.display = 'none';
+    actionsList2.style.display = 'flex';
+  } else {
+    actionsBlock.style.height = '140px';
+    actionsList.style.display = 'flex';
+    actionsList2.style.display = 'none';
+  }
+}
+
+updateLayout();
+
+
+window.addEventListener('resize', function() {
+  let bannersBlock = document.getElementById('banners-block');  // actions-list öğesini seç
+  let windowWidth = window.innerWidth;  // Pencere genişliğini al
+
+  if (windowWidth <= 767) {
+    bannersBlock.style.height= '775px';
+  } else {
+    bannersBlock.style.height = '270px';  // 767px üzeri için yükseklik 270px
+  }
+});
+
